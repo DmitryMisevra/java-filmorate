7128e6c817636f35fc2b40575b26914c8f977b50
Dmitry Misevra <shumi199@yandex.ru>
2023-08-14 12:55:20 +0600

    feat: добавить логирование в UserController

--- src/main/java/ru/yandex/practicum/filmorate/controllers/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controllers/UserController.java
@@ -2,7 +2,7 @@ package ru.yandex.practicum.filmorate.controllers;
 
 
 import jakarta.validation.Valid;
-import jakarta.validation.ValidationException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.User;
 
@@ -10,6 +10,7 @@ import java.util.*;
 
 @RestController
 @RequestMapping("/users")
+@Slf4j
 public class UserController {
 
     private int counter = 1;
@@ -23,7 +24,9 @@ public class UserController {
         user.setId(counter);
         users.put(counter, user);
         User newUser = users.get(counter);
+        log.debug("Добавили пользователя: {}", newUser);
         counter++;
+        log.debug("Состояние счетчика id пользователей: {}", counter);
         return newUser;
     }
 
@@ -33,19 +36,23 @@ public class UserController {
             validateLogin(user);
             setDefaultName(user);
             users.put(user.getId(), user);
+            log.debug("Обновили пользователя: {}", users.get(user.getId()));
             return users.get(user.getId());
         } else {
+            log.error("следующего id пользователя нет в списке: {}", user.getId());
             throw new ValidationException("Такого пользователя нет в системе");
         }
     }
 
     @GetMapping
     public List<User> getUsersList() {
+        log.debug("Текущее количество пользователей: {}", users.size());
         return new ArrayList<>(users.values());
     }
 
     private void validateLogin(User user) {
         if (user.getLogin().contains(" ")) {
+            log.error("в логине обнаружены пробелы: {}", user.getLogin());
             throw new ValidationException("Логин должен быть без пробелов");
         }
     }
@@ -53,6 +60,8 @@ public class UserController {
     private void setDefaultName(User user) {
         if (user.getName() == null || user.getName().isBlank()) {
             user.setName(user.getLogin());
+            log.debug("Имени присвоено значение логина. Значение логин: {}, значенрие имени:  {}", user.getLogin(),
+                    user.getName());
         }
     }
 }
