71388c4351a41bbe9ce5bf87830e304252cffcff
Dmitry Misevra <shumi199@yandex.ru>
2023-08-16 14:59:28 +0600

    refactor: добавить перезагрузку контекста после каждого теста

--- src/test/java/ru/yandex/practicum/filmorate/controllers/UserControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controllers/UserControllerTest.java
@@ -1,11 +1,11 @@
 package ru.yandex.practicum.filmorate.controllers;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.yandex.practicum.filmorate.exceptions.ValidationException;
 
@@ -17,20 +17,14 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 /* тестирование с помощью MockMVC */
+/* добавил перезагрузку контекста после каждого теста */
 @SpringBootTest
 @AutoConfigureMockMvc
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 class UserControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
-
-    @Autowired
-    private UserController userController;
-
-    @BeforeEach
-    void setUp() {
-        userController.clearUserController();
-    }
 
     @Test
     void addUser() throws Exception {
