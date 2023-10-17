da064ba17d3e9011e84fce7bcdffa1b2e7e06b3b
Dmitry Misevra <shumi199@yandex.ru>
2023-10-18 01:56:17 +0600

    feat: добавить поле userFriends для User

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -5,9 +5,10 @@ import lombok.Data;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.PastOrPresent;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Data
 @Builder
