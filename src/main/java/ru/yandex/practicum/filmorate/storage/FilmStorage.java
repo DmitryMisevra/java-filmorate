073cfed13790348d806996f927c16054c946031b
Dmitry Misevra <shumi199@yandex.ru>
2023-10-18 00:53:31 +0600

    feat: дополнить интерфейс FilmStorage методами для поиска жанров и рейтингов

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
@@ -1,6 +1,8 @@
 package ru.yandex.practicum.filmorate.storage;
 
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.Genre;
+import ru.yandex.practicum.filmorate.model.Mpa;
 
 import java.util.List;
 
@@ -16,8 +18,20 @@ public interface FilmStorage {
     Film updateFilm(Film film);
 
     /* находит фильм по id */
-    Film findFilmById(Long id);
+    Film findFilmById(long id);
 
     /* возвращает список сохраненных фильмов */
     List<Film> getFilmsList();
+
+    /* возвращает список всех жанров */
+    List<Genre> getGenresList();
+
+    /* возвращает жанр по id */
+    Genre findGenreByid(long id);
+
+    /* возвращает список всех рейтингов */
+    List<Mpa> getMpaList();
+
+    /* возвращает рейтинг по id */
+    Mpa findMpaByid(long id);
 }
